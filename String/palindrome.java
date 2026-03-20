8b32d64f2e31b7993be1e06cc13a790d1d2f8cac
Avinash Som <avinash.som17@gmail.com>
2026-03-20 14:04:30 +0530

    palindrome

---
+++ String/palindrome.java
@@ -0,0 +1,24 @@
+package String;
+import java.util.*;
+
+public class palindrome {
+    public static boolean isPalindrome(String s){
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i) != s.charAt(s.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+       String str = sc.next();
+    //    String str = "racecar";
+       boolean idx= isPalindrome(str);
+       if(idx == true){
+        System.out.println("yes");
+       }else{
+        System.out.println("no");
+       }
+    }
+}
