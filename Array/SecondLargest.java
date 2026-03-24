bb08dddde51d7c09f42af306aae2f124643e5d4d
Avinash Som <avinash.som17@gmail.com>
2026-03-24 18:40:15 +0530

    optimal

--- Array/SecondLargest.java
+++ Array/SecondLargest.java
@@ -1,17 +1,31 @@
 import java.util.*;
 public class SecondLargest {
+    // public static int second(int []nums){ //Brute
+    //     Arrays.sort(nums);
+    //     int largest=nums[nums.length-1];
+    //     for(int i = nums.length-2; i>=0; i--){
+    //         if(nums[i] < largest){
+    //             return nums[i];
+    //         }
+    //     }
+    //     return -1;
+    // }
     public static int second(int []nums){
-        Arrays.sort(nums);
-        int largest=nums[nums.length-1];
-        for(int i = nums.length-2; i>=0; i--){
-            if(nums[i] < largest){
-                return nums[i];
+        int Largest=nums[0];
+        int seclar = -1;
+        for(int i=1; i<nums.length-1; i++){
+            if(nums[i] > Largest){
+                seclar= Largest;
+                Largest=nums[i];
+            }
+            else if(nums[i] < Largest && seclar < nums[i]){
+                seclar= nums[i];
             }
         }
-        return -1;
+        return seclar;
     }
     public static void main(String [] args){
-        int []arr= {1,3,4,2,5,7,7};
+        int []arr= {1,3,4,2,5,7,7,0,9,8,7,9,9,9};
         int secondno = second(arr);
         System.out.println(secondno);
     }
